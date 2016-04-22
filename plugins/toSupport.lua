571f29e32954629e1f995474ebc6ffce6a24cef6
uckian1395 <kian.baghaie@gmail.com>
2016-04-22 11:36:41 +0430

    Update toSupport.lua

--- plugins/toSupport.lua
+++ plugins/toSupport.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return " The Support Invition Link : \n https://telegram.me/joinchat/DQoB8T2OSHspHUlnL8q7aA\n-------------------------------------\nChannel: @Black_CH"
+  return " ⚡️The Support Invition Link⚡️: \n https://https://telegram.me/joinchat/DQoB8T2CbfEwhMC2SsRv8g\n➖➖➖➖➖➖➖➖➖➖\nChannel: @AstroTeam_CH"
   end
 return {
   description = "shows support link", 
