8445b054ef0ada70e54a0939be7a840260f80e26
uckian1395 <kian.baghaie@gmail.com>
2016-04-22 12:41:22 +0430

    Create tools.lua

---
+++ tools.lua
@@ -0,0 +1,75 @@
+do
+
+function run(msg, matches)
+  return [[ 
+ 
+  ⭐️Astro tools list⭐️ :
+______________________
+👾Group problem👾
+〰〰〰〰〰〰〰
+••!adddeveloper
+اضافه کردن سازنده به گروه درصورت وجود مشکل
+••!addmanager
+اضافه کردن مدیریت ربات به گروه در صورت وجود مشکل
+______________________
+⛔️filter list⛔️
+〰〰〰〰〰〰〰 
+••!addword
+اضافه کردن کلمه به لیست فیلتر  کلمات
+••!remword
+پاک کردن کلمه از لیستر فیلتر کلمات
+••!badwords
+مشاهده لیست فیلتر کلمات
+••!clearbadwords
+پاک کردن کلمات فیلتر شده
+______________________
+Group password
+〰〰〰〰〰〰〰 
+••!setpass
+تنظیم پسورد برای گروه
+••!pass
+دریافت پسورد گروه
+••!join
+اضافه شدن به گروه بوسیله پسورد گروه
+______________________
+🔰Fun tools🔰
+______________________
+🔎search tools🔍
+〰〰〰〰〰〰〰 
+!google
+جست و جو در گوگل
+••!aparat
+جست و جو در آپارات
+••!insta
+جست و جو در اینستا گرام
+______________________
+••!qr
+تبدیل هر چیزی به کیو ار کد
+______________________
+••!time
+دریافت زمان منطقه مورد نظر
+______________________
+••!vc
+تبدیل متن به وویس
+______________________
+
+developer: @mrunusuall
+channel: @astroteam 
+G00D LUCK ^_^
+ ]]
+end
+
+return {
+  description = "Shows bot commands", 
+  usage = "!commans: Shows bot commands",
+  patterns = {
+    "^!tools$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright and edit; @MRunsusuall
+--Persian Translate; @MRunsusuall
+--ch : @Astroteam
+--کپی بدون ذکر منبع حرام است
