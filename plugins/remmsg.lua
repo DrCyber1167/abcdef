ac41d096e912890bcbdbe7d83825ca59160eb7aa
uckian1395 <kian.baghaie@gmail.com>
2016-04-22 13:03:24 +0430

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -3,16 +3,16 @@ for i=1, #result do
 delete_msg(result[i].id, ok_cb, false)
 end
 if tonumber(extra.con) == #result then
-send_msg(extra.chatid, ''..#result..'of recently chats deleted', ok_cb, false)
+send_msg(extra.chatid, ''..#result..'🗑of recently chats deleted🗑', ok_cb, false)
 else
-send_msg(extra.chatid, 'Super group chats deleted', ok_cb, false)
+send_msg(extra.chatid, '🗑Super group chats deleted🗑', ok_cb, false)
 end
 end
 local function run(msg, matches)
 if matches[1] == 'msgrem' and is_owner(msg) then
             if msg.to.type == 'channel' then
             if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 1 then
-            return "Invalid value value will be under 100"
+            return "Invalid value\nvalue range[1-99]"
             end
             get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
         else
