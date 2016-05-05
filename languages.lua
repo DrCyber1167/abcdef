d2374a4736c2af1b1d4ee9b9618e78c9dd6bdd68
uckian1395 <kian.baghaie@gmail.com>
2016-05-05 08:17:59 +0430

    Create languages.lua

---
+++ languages.lua
@@ -0,0 +1,16 @@
+return {
+    en = {
+        bonus = {
+
+     mods_list = 'List of the *group\'s moderators*:\n&&&1',
+                }
+     help{
+     owner = 'test',
+     moderator = 'tsst',
+     all = 'hdudj',
+     private = 'jjihh',
+    group_success = '_I\'ve sent you the help message in private_',
+    group_not_success = '_Please message me first so I can message you_',
+    initial = 'Choose the *role* to see the available commands:' 
+     },
+}
